55b30a70f5e0caebbb783bb567566b1b2c11f5f2
Inga Øien Mæhlum <inga.mahlum@icloud.com>
2021-09-09 13:38:03 +0200

    wetcake

--- wetcake.js
+++ wetcake.js
@@ -1,4 +1,9 @@
 const servings = 32;
 
-let egg=5/16*5;
-console.log("egg"+egg);
+let egg = 5/16*5;
+console.log("egg:"+egg);
+let sukker = 150/16*150;
+console.log("sukker:"+sukker);
+let hvetemel = 150/16*150;
+console.log("hvetemel:"+hvetemel);
+let bakepulver = 1/16*5;
